2fee2613e0b409f4bc96100fb196ad0cac141c72
Caaalabash <1121062986@qq.com>
2020-06-08 12:54:48 +0800

    test: 测试自建stun/turn服务器

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -65,7 +65,10 @@ export default {
         },
         createPeerConnection(stream, calleeId) {
             const pc = new RTCPeerConnection({
-                'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]
+                'iceServers': [
+                    {'urls': 'stun:111.229.115.45:3478'},
+                    {'urls': 'turn:111.229.115.45:3478', 'username': 'calabash', 'credential': 'calabashisbest' }
+                ],
             })
             for (const track of stream.getTracks()) {
                 pc.addTrack(track)
